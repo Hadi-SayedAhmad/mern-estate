9be545e3eada87a2cb76607e625dc853eb5e2660
Hadi S. Ahmad <hadi.github.acc@gmail.com>
2024-04-06 21:16:46 +0300

    Completed header initially.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Profile from './pages/Profile'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element='Home'></Route>
-        <Route path='/sign-in' element='SignIn'></Route>
-        <Route path='/sign-up' element='SignUp'></Route>
-        <Route path='/about' element='About'></Route>
-        <Route path='/profile' element='Profile'></Route>
-      </Routes> 
+      <Header />
+        <Routes>
+          <Route path="/" element="Home"></Route>
+          <Route path="/sign-in" element="SignIn"></Route>
+          <Route path="/sign-up" element="SignUp"></Route>
+          <Route path="/about" element="About"></Route>
+          <Route path="/profile" element="Profile"></Route>
+        </Routes>
+      
     </BrowserRouter>
-  )
+  );
 }
