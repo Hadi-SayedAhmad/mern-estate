5823d4e5c9a3a1ff120e07201278b21109acfea6
Hadi S. Ahmad <hadi.github.acc@gmail.com>
2024-04-11 19:20:14 +0300

    Added swiper to the listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Header from "./components/Header";
 import CreateListing from "./pages/CreateListing.jsx";
 import PrivateRoute from "./components/PrivateRoute.jsx";
 import UpdateListing from "./pages/UpdateListing.jsx";
+import Listing from "./pages/Listing.jsx";
 export default function App() {
   return (
     <BrowserRouter>
@@ -18,6 +19,7 @@ export default function App() {
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/about" element={<About />} />
+          <Route path="/listing/:id" element={<Listing />} />
           <Route element={<PrivateRoute />} >
             <Route path="/profile" element={<Profile />} />
             <Route path="/create-listing" element={<CreateListing />} />
---
+++ client/src/pages/Listing.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { useSelector } from "react-redux";
+import { Navigation, Autoplay, Pagination } from "swiper/modules";
+import "swiper/css/bundle";
+
+export default function Listing() {
+  SwiperCore.use([Autoplay, Navigation, Pagination]);
+
+  const params = useParams();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [listing, setListing] = useState(null);
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/listing/get/${params.id}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          return;
+        }
+        setListing(data);
+        setLoading(false);
+        setError(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchListing();
+  }, [params.id]);
+
+  return (
+    <main>
+      {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
+      {error && (
+        <p className="text-center my-7 text-2xl">Something went wrong!</p>
+      )}
+      {listing && !loading && !error && (
+        <>
+          <Swiper
+            navigation
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }} pagination
+          >
+            {listing.imageUrls.map((url, index) => (
+              <SwiperSlide key={index}>
+                <div
+                  className="h-[550px]"
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </>
+      )}
+    </main>
+  );
+}
