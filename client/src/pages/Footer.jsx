534ab10ed64646a19fc8974a6ca9c1f536d37b2b
Hadi S. Ahmad <hadi.github.acc@gmail.com>
2024-04-13 01:01:18 +0300

    .

--- client/src/pages/Footer.jsx
+++ client/src/pages/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <div className='text-center text-slate-700 p-5 mt-7 border-t-2'>
       Estates Platform. All rights reserved.
       <p>Developed by Hadi S. Ahmad</p>
-      <p>Let's Connect: <a href='www.linkedin.com/in/hadi-s-ahmad'>www.linkedin.com/in/hadi-s-ahmad</a></p>
+      <p>Let's Connect: <a href='www.linkedin.com/in/hadi-s-ahmad' target='blank' >www.linkedin.com/in/hadi-s-ahmad</a></p>
     </div>
   )
 }
